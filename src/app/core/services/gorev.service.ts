886430b5d87d31da8283214b500cc1ea4a237eef
hamitylmz92 <hmd62392@gmail.com>
2021-08-24 16:20:28 +0300

    Görev Görev listesi seçilen projeye göre firebase de where kullanılarak getirildi

--- src/app/core/services/gorev.service.ts
+++ src/app/core/services/gorev.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Gorev } from '../models/gorev';
+import { Proje } from '../models/proje';
 import { FirebaseService } from './firebase.service';
 
 @Injectable({
@@ -8,5 +9,13 @@ import { FirebaseService } from './firebase.service';
 })
 export class GorevService extends FirebaseService<Gorev> {
 
-  constructor(fireStore: AngularFirestore) { super(fireStore)}
+  constructor(fireStore: AngularFirestore,private fireStoreService: AngularFirestore) { super(fireStore)}
+
+  ProjeGorevListesi(proje:Proje){
+    return this.fireStoreService
+        .collection('gorev', (ref) =>
+          ref.where('projeId', '==', proje.id)
+        )
+        .snapshotChanges();
+  }
 }
--- src/app/modules/proje/pages/proje-list/proje-list.component.ts
+++ src/app/modules/proje/pages/proje-list/proje-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 import { Gorev } from 'src/app/core/models/gorev';
 import { Proje } from 'src/app/core/models/proje';
 import { ProjeHareket } from 'src/app/core/models/projeHareket';
@@ -20,13 +21,20 @@ export class ProjeListComponent implements OnInit {
   projeList: Proje[] = [];
   selectProje: Proje = new Proje();
   projeHareketList: ProjeHareket[] = [];
-
   gorev: Gorev = new Gorev();
-  gorevList:Gorev[]=[];
-  selectGorev!:Gorev[];
+  gorevList: Gorev[] = [];
+  selectGorev!: Gorev[];
+  beklemede: boolean = false;
+
+  durumList: Durum[] = [
+    { ad: 'Tümü', deger: 10 },
+    { ad: 'Beklemede', deger: 0 },
+    { ad: 'Tamamlandı', deger: 1 },
+  ];
+  selectDurum: Durum = new Durum();
+
   ngOnInit(): void {
     this.ProjeList();
-
   }
 
   ProjeList() {
@@ -40,42 +48,75 @@ export class ProjeListComponent implements OnInit {
   }
 
   ProjeGorevList(proje: Proje) {
-    this.gorevService.List('gorev').subscribe((rv) => {
-      this.gorevList=rv.map((p)=>{
-        const data=p.payload.doc.data() as Gorev;
-        data.id=p.payload.doc.id;
-        return data;
-      }).filter(x=>x.projeId==proje.id)
-      .sort((a,b)=>{
-        return +a.islemTarihi - +b.islemTarihi;
-      });
-    })
+    this.gorevService.ProjeGorevListesi(proje).subscribe((rv) => {
+      var durum = false;
 
+      if (this.selectDurum.deger == 0) durum = false;
+      else if (this.selectDurum.deger == 1) durum = true;
+
+      if (this.selectDurum.deger != 10) {
+        this.gorevList = rv
+          .map((p) => {
+            const data = p.payload.doc.data() as Gorev;
+            data.id = p.payload.doc.id;
+            return data;
+          })
+          .filter((x) => x.projeId == proje.id)
+          .filter((x) => x.yapilmaDurumu == durum)
+          .sort((a, b) => {
+            return +a.islemTarihi - +b.islemTarihi;
+          });
+      } else {
+        this.gorevList = rv
+          .map((p) => {
+            const data = p.payload.doc.data() as Gorev;
+            data.id = p.payload.doc.id;
+            return data;
+          })
+          .filter((x) => x.projeId == proje.id)
+          .sort((a, b) => {
+            return +a.islemTarihi - +b.islemTarihi;
+          });
+      }
+    });
   }
 
-  GorevDurumuDegistir(gorev:Gorev){
-    this.gorevService.Update(gorev,gorev.id,"gorev")
+  GorevDurumuDegistir(gorev: Gorev) {
+    this.gorevService.Update(gorev, gorev.id, 'gorev');
   }
 
-  GorevSil(gorev:Gorev){
-    this.gorevService.Delete(gorev.id,"gorev");
+  GorevSil(gorev: Gorev) {
+    this.gorevService.Delete(gorev.id, 'gorev');
   }
 
   GorevEkle() {
-    console.log(this.gorev.yapilacakIsAciklama);
-    this.gorev.atananKullaniciId = localStorage.getItem('kullaniciId') as string;
-    this.gorev.atayanKullaniciId = localStorage.getItem('kullaniciId') as string;
+    this.gorev.atananKullaniciId = localStorage.getItem(
+      'kullaniciId'
+    ) as string;
+    this.gorev.atayanKullaniciId = localStorage.getItem(
+      'kullaniciId'
+    ) as string;
     this.gorev.projeId = this.selectProje.id;
     this.gorev.islemTarihi = new Date();
     this.gorev.yapilmaDurumu = false;
+
     this.gorevService.Add(this.gorev, 'gorev');
-    this.gorev.yapilacakIsAciklama="";
+    this.gorev.yapilacakIsAciklama = '';
   }
 
-  GorevAciklamaDuzenle(gorev:Gorev,aciklama:string){
-    if(gorev.yapilacakIsAciklama!=aciklama){
-      gorev.yapilacakIsAciklama=aciklama;
-      this.gorevService.Update(gorev,gorev.id,"gorev")
+  GorevAciklamaDuzenle(gorev: Gorev, aciklama: string) {
+    if (gorev.yapilacakIsAciklama != aciklama) {
+      gorev.yapilacakIsAciklama = aciklama;
+      this.gorevService.Update(gorev, gorev.id, 'gorev');
     }
   }
+
+  BeklemedeOlanlar() {
+    this.ProjeGorevList(this.selectProje);
+  }
+}
+
+export class Durum {
+  deger!: number;
+  ad!: string;
 }
